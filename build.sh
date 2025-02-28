f0d8a2ba58fb7a622df1646b6c84b2f9dca5a375
Gyimah3 <gideongyimah19@gmail.com>
2025-02-28 17:23:38 +0000

    added-pyaudio

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-apt-get update
-apt-get install -y python3-dev portaudio19-dev python3-pyaudio
-pip install --upgrade pip
-pip install wheel setuptools
-pip install -r requirements.txt
+# Update package lists
+apt-get update && apt-get install -y \
+    python3-dev \
+    portaudio19-dev \
+    gcc \
+    libasound2-dev
+
+# Install Python dependencies
+pip install --upgrade pip setuptools wheel
+pip install --no-cache-dir -r requirements.txt
