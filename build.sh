70d8508679d9446182d9e2e30c09488226638aca
Gyimah3 <gideongyimah19@gmail.com>
2025-02-28 18:22:11 +0000

    initial-commit

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 # Update packages
 apt-get update
 
-# Install PortAudio development libraries and other necessary packages
+# Install PortAudio development libraries
+apt-get install -y portaudio19-dev python3-all-dev
 
 # Install Python dependencies
 pip install --upgrade pip
