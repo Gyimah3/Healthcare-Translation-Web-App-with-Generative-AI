1320e843d58f493cc2d6d5dcf46ddac0b0658a8c
Gyimah3 <gideongyimah19@gmail.com>
2025-02-28 17:21:46 +0000

    added-pyaudio

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install -y portaudio19-dev python3-all-dev
+apt-get install -y python3-dev portaudio19-dev python3-pyaudio
+pip install --upgrade pip
+pip install wheel setuptools
 pip install -r requirements.txt
