7d804ec38226983b6837b729f18f1fb096566a04
Gyimah3 <gideongyimah19@gmail.com>
2025-02-28 18:39:09 +0000

    initial-commit

--- build.sh
+++ build.sh
@@ -13,26 +13,55 @@
 # # portaudio19-dev
 
 #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
+
+# # Update packages
+# apt-get update -y
+
+# # Install PortAudio development libraries with explicit paths
+# apt-get install -y portaudio19-dev python3-all-dev pkg-config
+
+# # Update LD_LIBRARY_PATH to help find the libraries
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/usr/lib
+
+# # Make sure pkg-config can find PortAudio
+# export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
+ 
+# # Install Python dependencies
+# pip install --upgrade pip
+
+# # Install PyAudio first with specific compilation flags
+# pip install --global-option="build_ext" --global-option="-I/usr/include" --global-option="-L/usr/lib" pyaudio
+
+# # Install the rest of the requirements
+# pip install -r requirements.txt
+
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
-# Update packages
+# Update package list
 apt-get update -y
 
-# Install PortAudio development libraries with explicit paths
-apt-get install -y portaudio19-dev python3-all-dev pkg-config
+# Install essential build tools and dependencies
+apt-get install -y build-essential libasound2-dev python3-dev python3-setuptools python3-numpy python3-cffi
 
-# Update LD_LIBRARY_PATH to help find the libraries
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/usr/lib
+# Download and build PortAudio from source
+cd /tmp
+wget http://files.portaudio.com/archives/pa_stable_v190700_20210406.tgz
+tar -zxvf pa_stable_v190700_20210406.tgz
+cd portaudio
+./configure --with-alsa
+make
+make install
+ldconfig
 
-# Make sure pkg-config can find PortAudio
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
- 
-# Install Python dependencies
+# Update pip and install other requirements that don't include PyAudio
 pip install --upgrade pip
 
-# Install PyAudio first with specific compilation flags
-pip install --global-option="build_ext" --global-option="-I/usr/include" --global-option="-L/usr/lib" pyaudio
+# Now try to install PyAudio
+pip install pyaudio
 
-# Install the rest of the requirements
+# Install remaining requirements
 pip install -r requirements.txt
